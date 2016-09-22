1956c13134212ca6a88a1fc255ba3a8e28880f59
Matt Ruffner <mattruffner7@gmail.com>
2016-09-22 16:35:01 -0400

    made enclosure more shallow

--- cad/teensy_enclosure.js
+++ cad/teensy_enclosure.js
@@ -3,10 +3,10 @@
 function main() {
    
    // make our outer shell
-   var block = cube({size: [22,40,12]});
+   var block = cube({size: [22,40,9]});
    
    // hollow insize to teensy size
-   block = block.subtract(cube({size: [18,36,12]}).translate([2,2,2]));
+   block = block.subtract(cube({size: [18.1,36.1,9]}).translate([2,2,2]));
    
    block = block.subtract(cube({size: [10,8,2]}).rotateX(90).translate([6,2,4]));
    
